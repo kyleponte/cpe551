b2a4c932bafcf3906ed8f1122c557a71816b9121
Kyle <kyleponte07@gmail.com>
2025-09-10 11:49:52 -0400

    Wrote the program for part 3 of in class assignment 1

---
+++ inclass1/my_sub.py
@@ -0,0 +1,12 @@
+"""
+my_sub.py
+Assigns values to two variables and subtracts the second from the first
+Kyle Ponte
+kponte@stevens.edu
+9/10/25
+"""
+
+a = 15
+b = 7
+c = a - b # subtracts b from a
+print(c)
