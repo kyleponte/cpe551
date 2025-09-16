7cc513f55b55071c6b2298b8924a76d33ef0e78b
Kyle <kyleponte07@gmail.com>
2025-09-16 13:04:51 -0400

    Completed in class assignment 2

---
+++ inclass2/find_sum.py
@@ -0,0 +1,9 @@
+"""
+Accepts two numbers from the user and prints the sum
+"""
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2 # calculates sum of both numbers
+print(f"The sum of the two numbers is: {sum}")
